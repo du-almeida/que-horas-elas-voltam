0f4ba6261d7629104d809be03ea7ddc1e32b317f
DuAlmeida <99351100+du-almeida@users.noreply.github.com>
2024-08-30 16:56:54 -0300

    config tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,7 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+
   ],
   theme: {
     extend: {
@@ -16,5 +17,8 @@ const config: Config = {
     },
   },
   plugins: [],
+
+  
+
 };
 export default config;
